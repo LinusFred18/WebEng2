37baad543f3be2a47c1ce3afcce0a5f564d80720
77hrs <e.medwedkin@web.de>
2025-04-18 15:23:07 +0200

    DraggableMarker hinzugefügt

--- src/pages/map.jsx
+++ src/pages/map.jsx
@@ -1,14 +1,46 @@
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
+import { useEffect, useRef, useState } from 'react';
 
-// Optional: Standard Marker-Icons reparieren
+// Standard-Marker-Icons fixen
 delete L.Icon.Default.prototype._getIconUrl;
 L.Icon.Default.mergeOptions({
   iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png',
   iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
   shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
 });
+
+// Custom Hook für draggable Marker
+const DraggableMarker = () => {
+  const [position, setPosition] = useState([48.130064, 11.583815]);
+  const markerRef = useRef(null);
+
+  const eventHandlers = {
+    dragend() {
+      const marker = markerRef.current;
+      if (marker != null) {
+        const newPos = marker.getLatLng();
+        setPosition([newPos.lat, newPos.lng]);
+      }
+    },
+  };
+
+  return (
+    <Marker
+      draggable={true}
+      eventHandlers={eventHandlers}
+      position={position}
+      ref={markerRef}
+    >
+      <Popup>
+        <b>Verschieb mich!</b><br />
+        Latitude: {position[0].toFixed(6)}<br />
+        Longitude: {position[1].toFixed(6)}
+      </Popup>
+    </Marker>
+  );
+};
 
 const MapView = () => {
   return (
@@ -17,11 +49,15 @@ const MapView = () => {
         attribution='&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
+      {/* Fester Marker */}
       <Marker position={[51.505, -0.09]}>
         <Popup>
-          Hier ist ein Marker! 🎯
+          Hier ist ein fester Marker! 📍
         </Popup>
       </Marker>
+
+      {/* Verschiebbarer Marker */}
+      <DraggableMarker />
     </MapContainer>
   );
 };
