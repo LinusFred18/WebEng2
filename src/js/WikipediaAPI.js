2f466b9c0d7bf97dd744523ca45df6dfa6e35184
Mathidru <mddruica@gmail.com>
2025-04-18 11:54:08 +0200

    added the function to destructure the wikipedia api response json. Issue "Integration der Wikipedia-API #9"

--- src/js/WikipediaAPI.js
+++ src/js/WikipediaAPI.js
@@ -3,6 +3,7 @@ const fetch = require('node-fetch');
 const searchTerm = 'Berlin';
 const url = `https://en.wikipedia.org/w/api.php?origin=*&action=query&list=search&srsearch=${encodeURIComponent(searchTerm)}&format=json`;
 
+//This method fetches the wikipedia data, from the term defined in the "searchTerm" variable. it returns a json with the title, the snippet and the url
 async function fetchWikiData() {
   try {
     const response = await fetch(url);
@@ -15,10 +16,36 @@ async function fetchWikiData() {
     }));
 
     console.log(wikipediaData);
+    return wikipediaData;
   } catch (err) {
     console.error('Fehler beim Abrufen der Daten:', err);
+    return [];
+  }
+  
+}
+
+
+//this method destructures the wiki data given in the function call. At this point it just prints the components in the console.
+async function destructionWikiData(wikipediaData){
+  let number = 1;
+  for(let i = 0; i < number; i++){
+    let {title, snippet, url} = wikipediaData[i];
+    console.log(title);
+    console.log(snippet);
+    console.log(url);
   }
 
 }
 
-fetchWikiData();
+
+async function main() {
+  const data = await fetchWikiData();
+  await destructionWikiData(data);
+}
+
+main();
+
+//maybe you need to execute
+//npm uninstall node-fetch
+//npm install node-fetch@2
+//first
