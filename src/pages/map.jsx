5a3f248ba53e4e4ad1c7155777126b44a3cf912e
David F <davidfischer139@gmail.com>
2025-04-18 16:11:19 +0200

    cleaned up not required components from template

--- src/pages/map.jsx
+++ src/pages/map.jsx
@@ -44,7 +44,7 @@ const DraggableMarker = () => {
 
 const MapView = () => {
   return (
-    <MapContainer center={[47.666873, 9.444825]} zoom={13} scrollWheelZoom={true} style={{ height: '100vh', width: '100%' }}>
+    <MapContainer center={[47.666873, 9.444825]} zoom={13} scrollWheelZoom={true} style={{ height: '60vh', width: '100%' }}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
