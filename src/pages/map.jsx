9be5e56a686402adfdee14bf3efa44ccd10d1a4f
77hrs <e.medwedkin@web.de>
2025-04-26 19:41:56 +0200

    integrated gps marker

--- src/pages/map.jsx
+++ src/pages/map.jsx
@@ -1,9 +1,9 @@
-import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
-import { useEffect, useRef, useState } from 'react';
+import { useRef, useState, useEffect } from 'react';
+import useGPSLocation from './gps';  // import Hook
 
-// Standard-Marker-Icons fixen
 delete L.Icon.Default.prototype._getIconUrl;
 L.Icon.Default.mergeOptions({
   iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png',
@@ -11,7 +11,6 @@ L.Icon.Default.mergeOptions({
   shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
 });
 
-// Custom Hook für draggable Marker
 const DraggableMarker = () => {
   const [position, setPosition] = useState([48.150901, 11.571602]);
   const markerRef = useRef(null);
@@ -42,6 +41,45 @@ const DraggableMarker = () => {
   );
 };
 
+const GPSDraggableMarker = () => {
+  const { gpsLocation } = useGPSLocation();
+  const fallbackPosition = [47.666873, 9.444825];
+  const [position, setPosition] = useState(fallbackPosition);
+  const markerRef = useRef(null);
+
+  // Set GPS-Koordinaten, if avaible
+  useEffect(() => {
+    if (gpsLocation) {
+      setPosition([gpsLocation.latitude, gpsLocation.longitude]);
+    }
+  }, [gpsLocation]);
+
+  const eventHandlers = {
+    dragend() {
+      const marker = markerRef.current;
+      if (marker != null) {
+        const newPos = marker.getLatLng();
+        setPosition([newPos.lat, newPos.lng]);
+      }
+    },
+  };
+
+  return (
+    <Marker
+      draggable={true}
+      eventHandlers={eventHandlers}
+      position={position}
+      ref={markerRef}
+    >
+      <Popup>
+        <b>GPS Marker</b><br />
+        Latitude: {position[0].toFixed(6)}<br />
+        Longitude: {position[1].toFixed(6)}
+      </Popup>
+    </Marker>
+  );
+};
+
 const MapView = () => {
   return (
     <MapContainer center={[47.666873, 9.444825]} zoom={13} scrollWheelZoom={true} style={{ height: '60vh', width: '100%' }}>
@@ -49,14 +87,11 @@ const MapView = () => {
         attribution='&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      {/* Fester Marker */}
-      <Marker position={[47.666873, 9.444825]}>
-        <Popup>
-          Hier ist ein fester Marker! 📍
-        </Popup>
-      </Marker>
 
-      {/* Verschiebbarer Marker */}
+      {/* GPS Marker */}
+      <GPSDraggableMarker />
+
+      {/* Second Marker 2 */}
       <DraggableMarker />
     </MapContainer>
   );
