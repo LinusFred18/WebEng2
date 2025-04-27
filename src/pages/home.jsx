a6207e5c167bce24a0329f13ce2bf11e7585c14b
77hrs <e.medwedkin@web.de>
2025-04-27 16:01:03 +0200

    Revert "added routing with import problems (not worked)"
    This reverts commit 6bd8ea2f0c5b01e31f36e5ee6a82ac973e741e97.

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,42 +1,36 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import {
   Page,
+  Navbar,
+  NavLeft,
+  NavTitle,
+  NavTitleLarge,
+  NavRight,
+  Link,
+  Toolbar,
   Block,
   BlockTitle,
+  List,
+  ListItem,
   Button,
 } from 'framework7-react';
 
 import ReverseGeocoding from './reverseGeocoding';
 import MapView from './map';
-import Route from './route'; // Import der neuen Route-Komponente
 
-const HomePage = () => {
-  const routeRef = useRef(null);
+const HomePage = () => (
+  <Page name="home">
 
-  const handleRouteCalculation = () => {
-    if (routeRef.current) {
-      routeRef.current.calculateRoute();
-    }
-  };
+    {/* Page content */}
+    <MapView />
+    <ReverseGeocoding />
 
-  return (
-    <Page name="home">
-      <MapView routeRef={routeRef} />
-      <ReverseGeocoding />
+    <BlockTitle>Panels</BlockTitle>
+    <Block className="grid grid-cols-2 grid-gap">
+      <Button fill panelOpen="left">Left Panel</Button>
+      <Button fill panelOpen="right">Right Panel</Button>
+    </Block>
 
-      <Block strong>
-        <Button fill onClick={handleRouteCalculation}>
-          Route berechnen
-        </Button>
-      </Block>
-
-      <BlockTitle>Panels</BlockTitle>
-      <Block className="grid grid-cols-2 grid-gap">
-        <Button fill panelOpen="left">Left Panel</Button>
-        <Button fill panelOpen="right">Right Panel</Button>
-      </Block>
-    </Page>
-  );
-};
-
+  </Page>
+);
 export default HomePage;
--- src/pages/map.jsx
+++ src/pages/map.jsx
@@ -1,7 +1,8 @@
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
-import { useRef, useImperativeHandle, forwardRef } from 'react';
+import { useRef, useState, useEffect } from 'react';
+import useGPSLocation from './gps';  // import Hook
 
 delete L.Icon.Default.prototype._getIconUrl;
 L.Icon.Default.mergeOptions({
@@ -10,60 +11,90 @@ L.Icon.Default.mergeOptions({
   shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
 });
 
-const MapView = forwardRef((props, ref) => {
-  const gpsMarkerRef = useRef(null);
-  const simpleMarkerRef = useRef(null);
+const DraggableMarker = () => {
+  const [position, setPosition] = useState([48.150901, 11.571602]);
+  const markerRef = useRef(null);
 
-  useImperativeHandle(ref, () => ({
-    getMarkers() {
-      const gpsPos = gpsMarkerRef.current?.getLatLng();
-      const simplePos = simpleMarkerRef.current?.getLatLng();
-      return { gpsPos, simplePos };
-    }
-  }));
-
-  const initialGpsPosition = [47.666873, 9.444825];
-  const initialSimplePosition = [48.150901, 11.571602];
+  const eventHandlers = {
+    dragend() {
+      const marker = markerRef.current;
+      if (marker != null) {
+        const newPos = marker.getLatLng();
+        setPosition([newPos.lat, newPos.lng]);
+      }
+    },
+  };
 
   return (
-    <MapContainer
-      center={initialGpsPosition}
-      zoom={13}
-      scrollWheelZoom={true}
-      style={{ height: '60vh', width: '100%' }}
+    <Marker
+      draggable={true}
+      eventHandlers={eventHandlers}
+      position={position}
+      ref={markerRef}
     >
+      <Popup>
+        <b>Verschieb mich!</b><br />
+        Latitude: {position[0].toFixed(6)}<br />
+        Longitude: {position[1].toFixed(6)}
+      </Popup>
+    </Marker>
+  );
+};
+
+const GPSDraggableMarker = () => {
+  const { gpsLocation } = useGPSLocation();
+  const fallbackPosition = [47.666873, 9.444825];
+  const [position, setPosition] = useState(fallbackPosition);
+  const markerRef = useRef(null);
+
+  // Set GPS-Koordinaten, if avaible
+  useEffect(() => {
+    if (gpsLocation) {
+      setPosition([gpsLocation.latitude, gpsLocation.longitude]);
+    }
+  }, [gpsLocation]);
+
+  const eventHandlers = {
+    dragend() {
+      const marker = markerRef.current;
+      if (marker != null) {
+        const newPos = marker.getLatLng();
+        setPosition([newPos.lat, newPos.lng]);
+      }
+    },
+  };
+
+  return (
+    <Marker
+      draggable={true}
+      eventHandlers={eventHandlers}
+      position={position}
+      ref={markerRef}
+    >
+      <Popup>
+        <b>GPS Marker</b><br />
+        Latitude: {position[0].toFixed(6)}<br />
+        Longitude: {position[1].toFixed(6)}
+      </Popup>
+    </Marker>
+  );
+};
+
+const MapView = () => {
+  return (
+    <MapContainer center={[47.666873, 9.444825]} zoom={13} scrollWheelZoom={true} style={{ height: '60vh', width: '100%' }}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      
-      {/* GPS Marker */}
-      <Marker
-        draggable={true}
-        position={initialGpsPosition}
-        ref={gpsMarkerRef}
-      >
-        <Popup>
-          <b>GPS Marker</b><br />
-          Latitude: {initialGpsPosition[0]}<br />
-          Longitude: {initialGpsPosition[1]}
-        </Popup>
-      </Marker>
 
-      {/* Manueller Marker */}
-      <Marker
-        draggable={true}
-        position={initialSimplePosition}
-        ref={simpleMarkerRef}
-      >
-        <Popup>
-          <b>Manueller Marker</b><br />
-          Latitude: {initialSimplePosition[0]}<br />
-          Longitude: {initialSimplePosition[1]}
-        </Popup>
-      </Marker>
+      {/* GPS Marker */}
+      <GPSDraggableMarker />
+
+      {/* Second Marker 2 */}
+      <DraggableMarker />
     </MapContainer>
   );
-});
+};
 
 export default MapView;
